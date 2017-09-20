57ec1d35e9733ca58c3671142a772102e349b3cf
Netkas <30381982+netkas@users.noreply.github.com>
2017-09-20 16:10:37 -0300

    Update index.php
    Added a missing detail

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
 	$Config->Accounts = array('root' => 'admin'); // Set the user accounts you want
 	$Config->SecretKey = 'SDAHJ9832NJ8FD2UNF32FN9328FN8923F9328FHNJ'; // This SecretKey contains unicode chracters, 20 or more Characters.
 
-	$PHP_C = new php_c($Config); // Declare a PHP-C Object
+	$PHP_C = new php_c($Config); // Declare a PHP-C Object w/ the Configuration Object
 	$PHP_C->initialize(); // Initialize the PHP-C Script (This will end the session as well)
 ?>
