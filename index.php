fe5c7edacfba2a5b2b482019cda201b7ec76bdd4
Netkas <30381982+netkas@users.noreply.github.com>
2017-09-20 16:09:33 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?PHP
-	include('php-c.php');
+	include('php-c.php'); // Include the PHP-C Class
 
-	$Config = new php_c_config();
-	$Config->Accounts = array('root' => 'admin');
-	$Config->SecretKey = 'SDAHJ9832NJ8FD2UNF32FN9328FN8923F9328FHNJ';
+	$Config = new php_c_config(); // Declare a configuration object
+	$Config->Accounts = array('root' => 'admin'); // Set the user accounts you want
+	$Config->SecretKey = 'SDAHJ9832NJ8FD2UNF32FN9328FN8923F9328FHNJ'; // This SecretKey contains unicode chracters, 20 or more Characters.
 
-	$PHP_C = new php_c($Config);
-	$PHP_C->initialize();
+	$PHP_C = new php_c($Config); // Declare a PHP-C Object
+	$PHP_C->initialize(); // Initialize the PHP-C Script (This will end the session as well)
 ?>
